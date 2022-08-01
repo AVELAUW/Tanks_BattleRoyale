206594ef8a62ac501d492c16b34b6f23a8dc1cc5
AVELAUW <67564878+AVELAUW@users.noreply.github.com>
2022-07-31 20:59:35 -0700

    Update wall.py

--- ple/games/Tanks_BattleRoyale/wall.py
+++ ple/games/Tanks_BattleRoyale/wall.py
@@ -1,10 +1,9 @@
-__author__ = 'Batchu Vishal'
 from onBoard import OnBoard
 import pygame
 
 '''
-This class defines all our walls in the game.
-Currently not much is done here, but we can add traps to certain walls such as spiked walls etc to damage the player
+This class defines the walls in the game.
+If you want, you can add traps or powerups to walls such to damage or heal the player.
 '''
 
 
@@ -13,7 +12,7 @@ class Wall(OnBoard):
     def __init__(self, raw_image, position):
         super(Wall, self).__init__(raw_image, position)
 
-    # Update the ladder image
+    # Update the image (scale)
     def updateImage(self, raw_image):
         self.image = raw_image
-        self.image = pygame.transform.scale(self.image, (15, 15))
+        self.image = pygame.transform.scale(self.image, (32, 32))
