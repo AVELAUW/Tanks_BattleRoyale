1330cc2b0aa60acbdac297d64b1537bd94cb4674
AVELAUW <67564878+AVELAUW@users.noreply.github.com>
2022-08-01 10:19:11 -0700

    Update player.py

--- ple/games/Tanks_BattleRoyale/player.py
+++ ple/games/Tanks_BattleRoyale/player.py
@@ -19,22 +19,7 @@ class Player(Person):
     def setSpeed(self):
         return self.__speed
 
-    # This manages the players shell
-    def continuousUpdate(self, wallGroupList):
-        # Only run when the player has no shell on the map
-        
-        if self.onLadder == 0:
-            wallsCollided = self.checkCollision(wallGroupList)
-
-            # If the player is not jumping
-            if self.isJumping == 0:
-                # We move down a little and check if we collide with anything
-                self.updateY(2)
-                laddersCollided = self.checkCollision(ladderGroupList)
-                wallsCollided = self.checkCollision(wallGroupList)
-                self.updateY(-2)
-                # If we are not colliding with anything below, then we start a
-                # jump with 0 speed so that we just fall down
-                if len(wallsCollided) == 0 and len(laddersCollided) == 0:
-                    self.isJumping = 1
-                    self.currentJumpSpeed = 0
+    # Same function as person's checkCollision()
+    def continuousUpdate(self, colliderGroup):
+        Colliders = pygame.sprite.spritecollide(self, colliderGroup, False)
+        return Colliders
