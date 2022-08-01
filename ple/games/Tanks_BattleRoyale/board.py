245f38e7f24e98da838022b01ca1be979651221c
AVELAUW <67564878+AVELAUW@users.noreply.github.com>
2022-08-01 00:41:05 -0700

    Update board.py

--- ple/games/Tanks_BattleRoyale/board.py
+++ ple/games/Tanks_BattleRoyale/board.py
@@ -17,12 +17,13 @@ class Board(object):
     The generation of the level also happens in this class.
     '''
 
-    def __init__(self, width, height, rewards, _dir, lives):
+    def __init__(self, width, height, rewards, value, lives):
         self.__width = width
-        self.__actHeight = height
-        self.__height = self.__actHeight + 10
-        self.p1_lives = lives
-        self.p2_lives = lives
+        self.__height = height # + 10
+        self.value = value # Shell speed
+        self.lives = lives
+        self.p1_lives = self.lives
+        self.p2_lives = self.lives
         self.rewards = rewards
         self.cycles = 0 # For the tank animations
         self.p1_dir = 1 # player1 is on the left, facing right
@@ -32,199 +33,108 @@ class Board(object):
             "tank_left1": pygame.image.load(os.path.join(_dir, 'assets/tank_left1.png')).convert_alpha(),
             "tank_right1": pygame.image.load(os.path.join(_dir, 'assets/tank_right1.png')).convert_alpha(),
             "wood_block": pygame.image.load(os.path.join(_dir, 'assets/wood_block.png')).convert_alpha(),
-            "heart": pygame.image.load(os.path.join(_dir, 'assets/heart.png')).convert_alpha()
+            "lives": pygame.image.load(os.path.join(_dir, 'assets/lives.png')).convert_alpha(),
+            "heart": pygame.image.load(os.path.join(_dir, 'assets/heart.png')).convert_alpha(),
+            "shell_right2": pygame.image.load(os.path.join(_dir, 'assets/shell_right2.png')).convert_alpha(),
+            "shell_left2": pygame.image.load(os.path.join(_dir, 'assets/shell_left2.png')).convert_alpha(),
+            "shell_up2": pygame.image.load(os.path.join(_dir, 'assets/shell_up2.png')).convert_alpha(),
+            "shell_down2": pygame.image.load(os.path.join(_dir, 'assets/shell_down2.png')).convert_alpha()
         }
 
         self.white = (255, 255, 255)
 
-        '''
-        The map is essentially an array of 30x80 in which we store what each block on our map is.
-        1 represents a wall, 2 for a ladder and 3 for a coin.
-        '''
+        # The map is an array of 11x11 in which we store what each block on our map is.
+        # 1 represents a wall, 2 is a tank, 3 is a bullet, 4 is the lives, 5 is the hearts.
         self.map = []
-        # These are the arrays in which we store our instances of different
-        # classes
-        self.Players = []
-        self.Enemies = []
-        self.Allies = []
-        self.Coins = []
+        # These are the arrays in which we store our instances of different classes
+        self.Players = [None] * 2
         self.Walls = []
-        self.Ladders = []
-        self.Fireballs = []
-        self.Boards = []
-        self.FireballEndpoints = []
+        self.Shells = [None] * 2
+        self.Lives = [None] * 2
+        self.Hearts = [None] * 6
 
         # Resets the above groups and initializes the game for us
         self.resetGroups()
 
         # Initialize the instance groups which we use to display our instances
         # on the screen
-        self.fireballGroup = pygame.sprite.RenderPlain(self.Fireballs)
+        self.shellGroup = pygame.sprite.RenderPlain(self.Shells)
         self.playerGroup = pygame.sprite.RenderPlain(self.Players)
-        self.enemyGroup = pygame.sprite.RenderPlain(self.Enemies)
         self.wallGroup = pygame.sprite.RenderPlain(self.Walls)
-        self.ladderGroup = pygame.sprite.RenderPlain(self.Ladders)
-        self.coinGroup = pygame.sprite.RenderPlain(self.Coins)
-        self.allyGroup = pygame.sprite.RenderPlain(self.Allies)
-        self.fireballEndpointsGroup = pygame.sprite.RenderPlain(
-            self.FireballEndpoints)
+        self.livesGroup = pygame.sprite.RenderPlain(self.Lives)
+        self.heartsGroup = pygame.sprite.RenderPlain(self.Hearts)
 
     def resetGroups(self):
-        self.score = 0
-        self.lives = 3
+        self.cycles = 0 # For the tank animations
+        self.p1_dir = 1 # player1 is on the left, facing right
+        self.p2_dir = 3 # player2 is on the right, facing left
+        self.p1_lives = self.lives
+        self.p2_lives = self.lives
         self.map = []  # We will create the map again when we reset the game
         self.Players = [
-            Player(
-                self.IMAGES["still"],
-                (self.__width / 2,
-                 435),
-                15,
-                15)]
-        self.Enemies = [
-            MonsterPerson(
-                self.IMAGES["monster0"],
-                (100,
-                 117),
-                self.rng,
-                self._dir)]
-        self.Allies = [Person(self.IMAGES["princess"], (50, 48), 18, 25)]
-        self.Allies[0].updateWH(self.Allies[0].image, "H", 0, 25, 25)
-        self.Coins = []
+            Player1(self.IMAGES["tank_left1"], (1, int(self.__height / 2)), 32, 32),
+            Player2(self.IMAGES["tank_left1"], (self.__width - 33, int(self.__height / 2)), 32, 32)]
         self.Walls = []
-        self.Ladders = []
-        self.Fireballs = []
-        self.FireballEndpoints = [OnBoard(self.IMAGES["still"], (50, 440))]
+        self.Shells = []
+        self.Lives = [
+            Wall(self.IMAGES[lives], 32, 0, 32, 32]), # For player1
+            Wall(self.IMAGES[lives], self.__width - 32, 0, 32, 32]) # For player2
+        self.Hearts = [
+            Wall(self.IMAGES[lives], 32, 0, 32, 32), # player1 life1
+            Wall(self.IMAGES[lives], 64, 0, 32, 32), # player1 life2
+            Wall(self.IMAGES[lives], 96, 0, 32, 32]), # player1 life3
+            Wall(self.IMAGES[lives], self.__width - 64, 0, 32, 32]), # player2 life1
+            Wall(self.IMAGES[lives], self.__width - 96, 0, 32, 32]), # player2 life2
+            Wall(self.IMAGES[lives], self.__width - 128, 0, 32, 32]) # player2 life3
         self.initializeGame()  # This initializes the game and generates our map
         self.createGroups()  # This creates the instance groups
 
-    # Checks to destroy a fireball when it reaches its terminal point
-    def checkFireballDestroy(self, fireball):
-        if pygame.sprite.spritecollide(
-                fireball, self.fireballEndpointsGroup, False):
-            # We use indices on fireballs to uniquely identify each fireball
-            self.DestroyFireball(fireball.index)
+    # Checks to destroy a shell when it reaches its terminal point
+    def checkShellDestroy(self, shell):
+        if pygame.sprite.spritecollide(shell, self.playerGroup, False):
+            # We use indices on shells to uniquely identify each shell
+            self.DestroyShell(shell.index)
 
-    # Creates a new fireball and adds it to our fireball group
-    def CreateFireball(self, location, monsterIndex):
-        if len(self.Fireballs) < len(self.Enemies) * 5:
-            self.Fireballs.append(
-                Fireball(self.IMAGES["fireballright"], (location[0], location[1] + 15), len(self.Fireballs),
-                         2 + len(self.Enemies) / 2, self.rng, self._dir))
-            # Starts monster's animation
-            self.Enemies[monsterIndex].setStopDuration(15)
-            self.Enemies[monsterIndex].setPosition(
-                (self.Enemies[monsterIndex].getPosition()[0], self.Enemies[monsterIndex].getPosition()[1] - 12))
-            self.Enemies[monsterIndex].setCenter(
-                self.Enemies[monsterIndex].getPosition())
-            self.createGroups()  # We recreate the groups so the fireball is added
+    # Creates a new shell and add it to our shell group
+    def CreateShell(self, location, direction, playerIndex):
+        # Check if player already has a shell on the board
+        if self.Shells[playerIndex-1] == None:
+            if direction == 0: # UP
+                self.Shells[playerIndex-1](Shell(self.IMAGES["shell_up2"], (location[0], location[1]-32), self.value, direction))
+            if direction == 1: # RIGHT
+                self.Shells[playerIndex-1](Shell(self.IMAGES["shell_right2"], (location[0]+32, location[1]), self.value, direction))
+            if direction == 2: # DOWN
+                self.Shells[playerIndex-1](Shell(self.IMAGES["shell_down2"], (location[0], location[1]+32), self.value, direction))
+            if direction == 3: # LEFT
+                self.Shells[playerIndex-1](Shell(self.IMAGES["shell_left2"], (location[0]-32, location[1]), self.value, direction))
 
-    # Destroy a fireball if it has collided with a player or reached its
-    # endpoint
-    def DestroyFireball(self, index):
-        for fireBall in range(len(self.Fireballs)):
-            if self.Fireballs[fireBall].index == index:
-                self.Fireballs.remove(self.Fireballs[fireBall])
-                for fireBallrem in range(
-                        len(self.Fireballs)):  # We need to reduce the indices of all fireballs greater than this
-                    if self.Fireballs[fireBallrem].index > index:
-                        self.Fireballs[fireBallrem].index -= 1
-                self.createGroups()  # Recreate the groups so the fireball is removed
-                break
+    # Destroy a shell if it has collided with a player or hit a wall
+    def DestroyShell(self, playerIndex):
+        if playerIndex == 1:
+            self.Shells[0] = None
+        if playerIndex == 2:
+            self.Shells[1] = None
+        self.createGroups()  # Recreate the groups so the shell is removed
 
-    # Randomly Generate coins in the level where there is a wall below the
-    # coin so the player can reach it
-    def GenerateCoins(self):
-        for i in range(6, len(self.map)):
-            for j in range(len(self.map[i])):
-                if self.map[i][j] == 0 and ((i + 1 < len(self.map) and self.map[i + 1][j] == 1) or (
-                        i + 2 < len(self.map) and self.map[i + 2][j] == 1)):
-                    randNumber = math.floor(self.rng.rand() * 1000)
-                    if randNumber % 35 == 0 and len(
-                            self.Coins) <= 25:  # At max there will be 26 coins in the map
-                        self.map[i][j] = 3
-                        if j - 1 >= 0 and self.map[i][j - 1] == 3:
-                            self.map[i][j] = 0
-                        if self.map[i][j] == 3:
-                            # Add the coin to our coin list
-                            self.Coins.append(
-                                Coin(
-                                    self.IMAGES["coin1"],
-                                    (j * 15 + 15 / 2,
-                                     i * 15 + 15 / 2),
-                                    self._dir))
-        if len(
-                self.Coins) <= 15:  # If there are less than 21 coins, we call the function again
-            self.GenerateCoins()
-
-    # Given a position and checkNo ( 1 for wall, 2 for ladder, 3 for coin) the
-    # function tells us if its a valid position to place or not
-    def checkMapForMatch(self, placePosition, floor, checkNo, offset):
-        if floor < 1:
-            return 0
-        for i in range(
-                0, 5):  # We will get things placed atleast 5-1 blocks away from each other
-            if self.map[floor * 5 - offset][placePosition + i] == checkNo:
-                return 1
-            if self.map[floor * 5 - offset][placePosition - i] == checkNo:
-                return 1
-        return 0
-
-    # Create an empty 2D map of 30x80 size
+    # Create an empty 2D map of 11x11 size
     def makeMap(self):
-        for point in range(0, int(self.__height / 15 + 1)):
+        for point in range(int(self.__height / 32)):
             row = []
-            for point2 in range(0, int(self.__width / 15)):
+            for point2 in range(0, int(self.__width / 32)):
                 row.append(0)
             self.map.append(row)
 
-    # Add walls to our map boundaries and also the floors
+    # Add walls to our map boundaries
     def makeWalls(self):
-        for i in range(0, int(self.__height / 15)):
-            self.map[i][0] = self.map[i][int(self.__width / 15 - 1)] = 1
-        for i in range(2, int(self.__height / (15 * 4))):
-            for j in range(0, int(self.__width / 15)):
-                self.map[i * 5][j] = 1
-
-    # Make a small chamber on the top where the princess resides
-    def makePrincessChamber(self):
-        for j in range(0, 4):
-            self.map[j][9] = 1
-
-        for j in range(0, 10):
-            self.map[4][j] = 1
-
-        for j in range(0, 6):
-            self.map[1 * 4 + j][7] = self.map[1 * 4 + j][8] = 2
-
-    # Generate ladders randomly, 1 for each floor such that they are not too
-    # close to each other
-    def makeLadders(self):
-        for i in range(2, int(self.__height / (15 * 4) - 1)):
-            ladderPos = math.floor(self.rng.rand() * (self.__width / 15 - 20))
-            ladderPos = int(7 + ladderPos)
-            while self.checkMapForMatch(ladderPos, i - 1, 2, 0) == 1:
-                ladderPos = math.floor(
-                    self.rng.rand() * (self.__width / 15 - 20))
-                ladderPos = int(7 + ladderPos)
-            for k in range(0, 5):
-                self.map[i * 5 + k][ladderPos] = self.map[i *
-                                                          5 + k][ladderPos + 1] = 2
-
-    # Create the holes on each floor (extreme right and extreme left)
-    def makeHoles(self):
-        for i in range(3, int(self.__height / (15 * 4) - 1)):
-            for k in range(
-                    1, 6):  # Ladders wont interfere since they leave 10 blocks on either side
-                if i % 2 == 0:
-                    self.map[i * 5][k] = 0
-                else:
-                    self.map[i * 5][int(self.__width / 15 - 1 - k)] = 0
+        for i in range(int(self.__height / 32)):
+            self.map[i][0] = self.map[i][int(self.__width / 32)] = 1
+        for j in range(int(self.__width / 32)):
+            self.map[0][j] = self.map[int(self.__width / 32)][j] = 1
 
     '''
-    This is called once you have finished making holes, ladders, walls etc
+    This is called once you have finished making the game field
     You use the 2D map to add instances to the groups
     '''
-
     def populateMap(self):
         for x in range(len(self.map)):
             for y in range(len(self.map[x])):
@@ -242,20 +152,6 @@ class Board(object):
                             self.IMAGES["ladder"],
                             (y * 15 + 15 / 2,
                              x * 15 + 15 / 2)))
-
-    # Check if the player is on a ladder or not
-    def ladderCheck(self, laddersCollidedBelow,
-                    wallsCollidedBelow, wallsCollidedAbove):
-        if laddersCollidedBelow and len(wallsCollidedBelow) == 0:
-            for ladder in laddersCollidedBelow:
-                if ladder.getPosition()[1] >= self.Players[0].getPosition()[1]:
-                    self.Players[0].onLadder = 1
-                    self.Players[0].isJumping = 0
-                    # Move the player down if he collides a wall above
-                    if wallsCollidedAbove:
-                        self.Players[0].updateY(3)
-        else:
-            self.Players[0].onLadder = 0
 
     # Update all the fireball positions and check for collisions with player
     def fireballCheck(self):
@@ -309,7 +205,7 @@ class Board(object):
             # Create the groups again so the enemies are effected
             self.createGroups()
 
-    # Redraws the entire game screen for us
+    # Redraws the entire game screen
     def redrawScreen(self, screen, width, height):
         screen.fill((40, 20, 0))  # Fill it with black
         # Draw all our groups on the background
