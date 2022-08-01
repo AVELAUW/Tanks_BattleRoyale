4359eabc74dad050abab15333414630091d3ab47
AVELAUW <67564878+AVELAUW@users.noreply.github.com>
2022-07-31 21:56:44 -0700

    Update shell.py

--- ple/games/Tanks_BattleRoyale/shell.py
+++ ple/games/Tanks_BattleRoyale/shell.py
@@ -1,134 +1,119 @@
-__author__ = 'Erilyth'
 import pygame
 import math
 import os
-from .onBoard import OnBoard
+from .environment import Environment
 
 '''
-This class defines all our fireballs.
-A fireball inherits from the OnBoard class since we will use it as an inanimate object on our board.
-Each fireball can check for collisions in order to decide when to turn and when they hit a player.
+This class defines the tank's shells.
+A shell inherits from the environment class since we will use it as an inanimate object on our board.
+Each shell checks for collisions with the wood boxes in order to decide whether they keep going.
 '''
 
-
-class Shell(OnBoard):
-
-    def __init__(self, raw_image, position, index, speed, rng, dir):
+class Shell(Environment):
+    def __init__(self, raw_image, position, speed, dir):
         super(Shell, self).__init__(raw_image, position)
-        # Set the fireball direction randomly
-        self.rng = rng
-        self.__direction = int(math.floor(self.rng.rand() * 100)) % 2
-        self.index = index
-        self.wallsBelow = []
-        self.laddersBelow = []
+        # Set the shell direction to the direction it was shot from
+        self.__direction = dir
 
         self.IMAGES = {
-            "fireballright": pygame.transform.scale(pygame.image.load(os.path.join(dir, 'assets/fireballright.png')), (20, 20)).convert_alpha(),
-            "fireballleft": pygame.transform.scale(pygame.image.load(os.path.join(dir, 'assets/fireballleft.png')), (20, 20)).convert_alpha()
+            "shell_right1": pygame.transform.scale(pygame.image.load(os.path.join(dir, 'assets/shell_right1.png')), (8, 8)).convert_alpha(),
+            "shell_right2": pygame.transform.scale(pygame.image.load(os.path.join(dir, 'assets/shell_right2.png')), (8, 8)).convert_alpha(),
+            "shell_up1": pygame.transform.scale(pygame.image.load(os.path.join(dir, 'assets/shell_up1.png')), (8, 8)).convert_alpha(),
+            "shell_up2": pygame.transform.scale(pygame.image.load(os.path.join(dir, 'assets/shell_up2.png')), (8, 8)).convert_alpha(),
+            "shell_left1": pygame.transform.scale(pygame.image.load(os.path.join(dir, 'assets/shell_left1.png')), (8, 8)).convert_alpha(),
+            "shell_left2": pygame.transform.scale(pygame.image.load(os.path.join(dir, 'assets/shell_left2.png')), (8, 8)).convert_alpha(),
+            "shell_down1": pygame.transform.scale(pygame.image.load(os.path.join(dir, 'assets/shell_down1.png')), (8, 8)).convert_alpha(),
+            "shell_down2": pygame.transform.scale(pygame.image.load(os.path.join(dir, 'assets/down2.png')), (20, 20)).convert_alpha(),
+            "boom3": pygame.transform.scale(pygame.image.load(os.path.join(dir, 'assets/boom3.png')), (16, 16)).convert_alpha()
         }
-        # The newly spawned fireball is not falling
-        self.__fall = 0
-        # The speed of a fireball is set
+        # The speed of a shell is set
         self.__speed = speed
 
-    # Update the image of a fireball
+    # Update the image of a shell
     def updateImage(self, raw_image):
         self.image = raw_image
 
-    # Getters and Setters for some private variables
+    # Getters and Setters for private variables
     def getSpeed(self):
         return self.__speed
 
     def setSpeed(self, speed):
         self.__speed = speed
 
-    def getFall(self):
-        return self.__fall
-
     def getDirection(self):
         return self.__direction
 
-    # Moves the fireball in the required direction
-    def continuousUpdate(self, wallGroup, ladderGroup):
+    # Move the shell in the required direction
+    def continuousUpdate(self, wallGroup):
+        # We are moving UP, so update the shell's image upwards
+        if self.__direction == 0:
+            self.update(self.IMAGES["shell_up1"], self.__speed)
+            # When we hit a wall, we explode
+            if self.checkCollision(wallGroup):
+                self.update(self.IMAGES["shell_up2"], 0)
+        # We are moving RIGHT, so update the shell's image to the right        
+        if self.__direction == 1:
+            self.update(self.IMAGES["shell_right1"], self.__speed)
+            # When we hit a wall, we explode
+            if self.checkCollision(wallGroup):
+                self.update(self.IMAGES["shell_right2"], 0)
+        # We are moving DOWN, so update the shell's image downards        
+        if self.__direction == 2:
+            self.update(self.IMAGES["shell_down1"], self.__speed)
+            # When we hit a wall, we explode
+            if self.checkCollision(wallGroup):
+                self.update(self.IMAGES["shell_down2"], 0)
+        # We are moving LEFT, so update the shell's image to the left
+        if self.__direction == 3:
+            self.update(self.IMAGES["shell_left1"], self.__speed)
+            # When we hit a wall, we explode
+            if self.checkCollision(wallGroup):
+                self.update(self.IMAGES["shell_left2"], 0)
 
-        # The fireball is falling
-        if self.__fall == 1:
-            # We move the fireball downwards with speed of self.__speed
-            self.update(self.image, "V", self.__speed)
-            if self.checkCollision(wallGroup, "V"):
-                # We have collided with a wall below, so the fireball can stop
-                # falling
-                self.__fall = 0
-                # Set the direction randomly
-                self.__direction = int(math.floor(self.rng.rand() * 100)) % 2
-
-        else:
-
-            # While we are on the ladder, we use a probability of 4/20 to make
-            # the fireball start falling
-            if self.checkCollision(ladderGroup, "V") and len(
-                    self.checkCollision(wallGroup, "V")) == 0:
-                randVal = int(math.floor(self.rng.rand() * 100)) % 20
-                if randVal < 15:
-                    self.__fall = 0
-                else:
-                    self.__fall = 1
-
-            # We are at the edge of the floor so the fireball starts falling
-            if len(self.checkCollision(ladderGroup, "V")) == 0 and len(
-                    self.checkCollision(wallGroup, "V")) == 0:
-                self.__fall = 1
-
-            # We are moving right, so update the fireball image to the right
-            if self.__direction == 0:
-                self.update(self.IMAGES["fireballright"], "H", self.__speed)
-                # When we hit a wall, we change direction
-                if self.checkCollision(wallGroup, "H"):
-                    self.__direction = 1
-                    self.update(self.image, "H", -self.__speed)
-
-            # We are moving left, so update the fireball image to the left
-            else:
-                self.update(self.IMAGES["fireballleft"], "H", -self.__speed)
-                # When we hit a wall, we change direction
-                if self.checkCollision(wallGroup, "H"):
-                    self.__direction = 0
-                    self.update(self.image, "H", self.__speed)
-
-    # Move the fireball in the required direction with the required value and
-    # also set the image of the fireball
-    def update(self, raw_image, direction, value):
-        if direction == "H":
+    # Move the shell in the required direction with the required speed (value), then set its image
+    def update(self, raw_image, value):
+        if self.__direction == 0: # UP
+            self.setPosition(
+                (self.getPosition()[0],
+                 self.getPosition()[1] - value))
+            self.image = raw_image
+        if self.__direction == 1: # RIGHT
             self.setPosition(
                 (self.getPosition()[0] + value,
                  self.getPosition()[1]))
             self.image = raw_image
-        if direction == "V":
+        if self.__direction == 2: # DOWN
             self.setPosition(
                 (self.getPosition()[0],
                  self.getPosition()[1] + value))
+            self.image = raw_image
+        if self.__direction == 3: # LEFT
+            self.setPosition(
+                (self.getPosition()[0] - value,
+                 self.getPosition()[1]))
+            self.image = raw_image
         self.rect.center = self.getPosition()
 
     '''
-    We check for collisions in the direction in which we are moving if the parameter direction is "H".
-    The way we do this is move a little forward in the direction in which we are moving, then check for collisions then move back to the original location
-    We check for collisions below the fireball if the parameter direction is "V"
-    We do this by moving down a little, then check for collisions then move back up to the original location
+    We check for collisions in the direction the shell is moving by moving forward a little,
+    checking for collisions, then moving back to the shell's original location.
     '''
-
-    def checkCollision(self, colliderGroup, direction):
-        if direction == "H":
-            if self.__direction == 0:
-                self.update(self.image, "H", self.__speed)  # Right collision
-            if self.__direction == 1:
-                self.update(self.image, "H", -self.__speed)  # Left collision
-            Colliders = pygame.sprite.spritecollide(self, colliderGroup, False)
-            if self.__direction == 0:
-                self.update(self.image, "H", -self.__speed)  # Right collision
-            if self.__direction == 1:
-                self.update(self.image, "H", self.__speed)  # Left collision
-        else:
-            self.update(self.image, "V", self.__speed)  # Bottom collision
-            Colliders = pygame.sprite.spritecollide(self, colliderGroup, False)
-            self.update(self.image, "V", -self.__speed)
+    def checkCollision(self, colliderGroup):        
+        if self.__direction == 0:
+            self.update(self.image, self.__speed) # check UP collision
+        if self.__direction == 1:
+            self.update(self.image, self.__speed) # check RIGHT collision
+        if self.__direction == 2:
+            self.update(self.image, self.__speed) # check DOWN collision
+        if self.__direction == 3:
+            self.update(self.image, self.__speed) # check LEFT collision    
+        Colliders = pygame.sprite.spritecollide(self, colliderGroup, False)
+        if self.__direction == 0:
+            self.update(self.image, -self.__speed) # reverese the UP movement
+        if self.__direction == 1:
+            self.update(self.image, -self.__speed) # reverse the RIGHT movement
+        if self.__direction == 2:
+            self.update(self.image, -self.__speed) # reverse the DOWN movement
+        if self.__direction == 3:
+            self.update(self.image, -self.__speed) # reverese the LEFT movement 
         return Colliders
