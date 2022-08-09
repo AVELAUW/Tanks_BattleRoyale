8821850650dca00a731e83b5985b2e3abcafff8b
AVELAUW <67564878+AVELAUW@users.noreply.github.com>
2022-08-09 00:17:13 -0700

    Update person.py

--- ple/games/Tanks_BattleRoyale/person.py
+++ ple/games/Tanks_BattleRoyale/person.py
@@ -18,6 +18,17 @@ class Person(pygame.sprite.Sprite):
             self.image, (width, height)).convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.center = self.__position
+        self._dir = os.path.dirname(os.path.abspath(__file__))
+        self.IMAGES = {
+            "tank_down1": pygame.image.load(os.path.join(self._dir, 'assets/tank_down_1.png')),
+            "tank_down2": pygame.image.load(os.path.join(self._dir, 'assets/tank_down_2.png')),
+            "tank_left1": pygame.image.load(os.path.join(self._dir, 'assets/tank_left_1.png')),
+            "tank_left2": pygame.image.load(os.path.join(self._dir, 'assets/tank_left_2.png')),
+            "tank_right1": pygame.image.load(os.path.join(self._dir, 'assets/tank_right_1.png')),
+            "tank_right2": pygame.image.load(os.path.join(self._dir, 'assets/tank_right_2.png')),
+            "tank_up1": pygame.image.load(os.path.join(self._dir, 'assets/tank_up1.png')),
+            "tank_up2": pygame.image.load(os.path.join(self._dir, 'assets/tank_up2.png'))
+        }
 
     '''
     We set these as abstract methods since this class does not have a speed variable set, but we want all the child classes to
@@ -42,19 +53,26 @@ class Person(pygame.sprite.Sprite):
 
     # Move the person in the horizontal (%2==1) or vertical (%2==0) axis
     def update(self, raw_image, direction, value, width, height):
-        '''
         if direction == 0: # UP
             self.__position = (self.__position[0], self.__position[1] - value)
+            self.updateImage(self.IMAGES["tank_up1"])
         if direction == 1: # RIGHT
             self.__position = (self.__position[0] + value, self.__position[1])
+            self.updateImage(self.IMAGES["tank_right1"])
         if direction == 2: # DOWN
             self.__position = (self.__position[0], self.__position[1] + value)
+            self.updateImage(self.IMAGES["tank_down1"])
         if direction == 3: # LEFT
             self.__position = (self.__position[0] - value, self.__position[1])
-        '''
+            self.updateImage(self.IMAGES["tank_left1"])
         # Update the image to the specified width and height 
         self.image = pygame.transform.scale(self.image, (width, height))
         self.rect.center = self.__position
+        
+    # Update the image of a person (and scale)
+    def updateImage(self, raw_image):
+        self.image = raw_image
+        self.image = pygame.transform.scale(self.image, (32, 32))
 
     # Given a collider list, just check if the person instance collides with
     # any of them
