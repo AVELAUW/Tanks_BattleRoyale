8556176932b2acf533a9787684ef24138c5d00ec
AVELAUW <67564878+AVELAUW@users.noreply.github.com>
2022-08-09 11:27:58 -0700

    Update board.py

--- ple/games/Tanks_BattleRoyale/board.py
+++ ple/games/Tanks_BattleRoyale/board.py
@@ -189,13 +189,13 @@ class Board(object):
                 #self.Players[1].setPosition((self.__width - 64+self.adjust, int(self.__height / 2))) 
                 self.RemoveHeart(2) # Player 2 hit
                 self.createGroups()
-                print(self.playerGroup, len(self.playerGroup))
+                print(self.heartsGroup, len(self.heartsGroup))
             if shell.index == 2 and shell.checkCollision(self.playerGroup):
                 self.Shells.remove(shell)
                 #self.Players[0].setPosition((32+self.adjust, int(self.__height / 2)))
                 self.RemoveHeart(1) # Player 1 hit
                 self.createGroups()
-                print(self.playerGroup, len(self.playerGroup))
+                print(self.heartsGroup, len(self.heartsGroup))
             self.checkShellDestroy(shell)
     
     # Check if the player wins
